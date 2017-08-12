8a3ee1beb65851c405df61a8ecb68a9fac8641bf
Tim Yang <timyang@tim-yang-mba.local>
2017-08-11 21:45:40 -0700

    Add to test cases for GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const app = require('./../server.js').app;
 const Todo = require('./../models/todo.js').Todo;
 
 const todos = [{
+    _id: new ObjectID(),
     text: 'first test todo'
 }, {
+    _id: new ObjectID(),
     text: 'second test todo'
 }];
 
@@ -67,6 +70,33 @@ describe('POST /todos', () => {
                 })
                 .end(done);
                 
+        });
+    });
+
+    describe('GET /todos/:id', () => {
+        it('should return to doc', (done) => {
+            request(app)
+                .get(`/todos/${todos[0]._id.toHexString()}`)
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body.todo.text).toBe(todos[0].text);
+                })
+                .end(done);
+                
+        });
+        it('should return 404 if todo not found', (done) => {
+            var hexID = new ObjectID().toHexString();
+            request(app)
+            .get(`/todos/${hexID}`)
+            .expect(404)
+            .end(done);
+        });
+
+        it('should return 404 for non-object ids', (done) => {
+            request(app)
+            .get(`/todos/123`)
+            .expect(404)
+            .end(done);
         });
     });
 
