8ffaf61a3bcdffec17667f0f03d737346e8dac5c
Tim Yang <timyang@tim-yang-mba.local>
2017-08-11 22:42:19 -0700

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -55,6 +55,27 @@ app.get('/todos/:id', (req, res) => {
 
 });
 
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({
+            todo: todo
+        });
+        
+    }).catch((e) => {
+        res.status(400).send();
+    })
+
+});
+
 app.listen(port), ()=> {
     console.log(`Started on port ${port}`);
 };
