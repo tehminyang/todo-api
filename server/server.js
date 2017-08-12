3786c8416caec4ea5e575d89ba5865d6736201aa
Tim Yang <timyang@tim-yang-mba.local>
2017-08-11 21:30:42 -0700

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var mongoose = require('./db/mongoose.js').mongoose;
 var Todo = require('./models/todo.js').Todo;
@@ -32,6 +33,26 @@ app.get('/todos', (req, res) => {
     });
 });
 
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({
+            todo: todo
+        });
+        
+    }).catch((e) => {
+        res.status(404).send();
+    })
+
+});
 
 app.listen(3000), ()=> {
     console.log('Started on port 3000');
