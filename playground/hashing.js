3e51167348904ecfcfa882a13b18d44ed76f66e0
Tim Yang <timyang@tim-yang-mba.local>
2017-08-12 23:04:43 -0700

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,28 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded:', decoded);
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     });
+// });
+
+var hashedpw = '$2a$10$om3zSffgPLa62Sl/HezLgOfGOZHV.548Cl4UhKy0Kdsmei9q399La';
+
+bcrypt.compare(password, hashedpw, (err, res) => {
+    console.log(res);
+});
+// var data = {
+//     id: 10
+// };
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded:', decoded);
 
 
 
